2a7cdd88ecdb67301694335a31bf9b1878f2b073
skyroam <skyroam.wyx@gmail.com>
2020-01-30 17:46:23 +0800

    add

--- crazy_java/chapter4/Test2.java
+++ crazy_java/chapter4/Test2.java
@@ -7,6 +7,7 @@ public class Test2
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        System.out.println("Please input a number:");
         int num = in.nextInt();
         for(int i = 0; i < num; i++)
         {
--- crazy_java/chapter4/Test3.java
+++ crazy_java/chapter4/Test3.java
@@ -1,9 +1,55 @@
 package chapter4;
 
-public class Test4
+import java.util.Scanner;
+
+
+public class Test3
 {
     public static void main(String[] args) 
     {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please input a number:");
+        float radius = in.nextFloat();
+        //float diameter = 2 * radius;
+ 
+        for(float j = radius; j > 0; j--)
+        {    
+            double dis = Math.sqrt(radius*radius - j*j);
+            for(int i = 0; i < (int)(2*radius); i ++)
+            {   
+                //System.out.println(i);
+                //System.out.println(radius-dis);
+                //System.out.println(radius+dis);
+                if(i == (int)(radius-dis) || i == (int)(radius+dis)) 
+                {
+                    System.out.print('*');
+                }
+                else
+                {
+                    System.out.print(' ');
+                }
+                
+            }
+            System.out.println();
+        }
+
+        for(float j = 1; j < radius; j++)
+        {    
+            double dis = Math.sqrt(radius*radius - j*j);
+            for(int i = 0; i < (int)(2*radius); i ++)
+            {
+                if(i == (int)(radius-dis) || i == (int)(radius+dis)) 
+                {
+                    System.out.print('*');
+                }
+                else
+                {
+                    System.out.print(' ');
+                }
+                
+            }
+            System.out.println();
+        }
 
     }
 }
